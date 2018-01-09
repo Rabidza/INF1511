f4287a4437efd3aa3af8dfe7c1468c92726d5e82
Neill Henning <neillhenning@gmail.com>
2018-01-09 21:59:25 +0200

    Cleanup -- Move files to their respective folders

---
+++ Chapter 2/octhex.py
@@ -0,0 +1,10 @@
+a = 0o25
+b = 0x1af
+print('Value of a in decimal is', a)
+
+c = 19
+print('19 in octal is %o and in hex is %x' %(c, c))
+
+d = oct(c)
+e = hex(c)
+print ('19 in octal is', d, 'and in hexa is', e)
