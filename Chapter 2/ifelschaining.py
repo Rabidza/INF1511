83409521131eb9e5e2caa0bbb78270d59ae9ab86
Neill Henning <neillhenning@gmail.com>
2018-02-11 13:52:52 +0200

    Making Decisions if, elif else examples.

---
+++ Chapter 2/ifelschaining.py
@@ -0,0 +1,8 @@
+m = int(input("Enter grades: "))
+
+if m >= 60:
+    print("First Division")
+if 45 <= m < 60:
+    print("Second Division")
+if m < 45:
+    print("Third Division")
---
+++ Chapter 2/ifelse3.py
@@ -0,0 +1,8 @@
+m = int(input("Enter grades: "))
+
+if m >= 60:
+    print("First Division")
+if m >=45 and m < 60:  # Will be simplified in next exercise.
+    print("Second Division")
+if m < 45:
+    print("Third Division")
