67c1171c88ef02026982660bb6544151d447cdd2
Neill Henning <neillhenning@gmail.com>
2018-02-27 22:38:58 +0200

    docstring example

---
+++ Chapter04/docstr.py
@@ -0,0 +1,8 @@
+def rect(l, b):
+    """Computes the area of rectangle
+    Values for length and breadth are passed to the function for computation"""
+    print("Area of rectangle is", l * b)
+
+
+rect(5, 8)
+print(rect.__doc__)
